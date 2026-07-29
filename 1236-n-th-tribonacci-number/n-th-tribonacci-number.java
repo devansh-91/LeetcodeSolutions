4f8d782ebc8581ee302d34ea3c0929a1b2fe9375
Devansh Mishra <official.devansh91@gmail.com>
2026-07-29 10:17:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (15.68%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -2,9 +2,7 @@ class Solution {
     public int tribonacci(int n) {
         if(n<=0)
             return 0;
-        if(n==1)
-            return 1;
-        if(n==2)
+        if(n==1 || n==2)
             return 1;
         int t0=0,t1=1,t2=1,t3=0;
         n=n-2;
